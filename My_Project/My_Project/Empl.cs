35ae198fd4561e8e0b2a267e2ef1525f06b4835b
Bakurov <w.cr1t1c@gmail.com>
2015-02-25 23:25:33 +0300

    Initial commit

---
+++ My_Project/My_Project/Empl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace My_Project
+{
+    public abstract class Empl
+    {
+        
+        protected Empl(string p)
+        {
+            _post = p;
+        }
+
+        public string _post;
+
+        public virtual void ShowEmpl()
+        {
+            Console.WriteLine("Сотрудник");
+        }
+    }
+}
